290819853c03e72de7c0f58339806ac16603429a
rafael marcilio <rafaelbatistamarcilio@gmail.com>
2021-01-25 20:00:33 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/app/exemploddd/ExemplodddApplication.java
@@ -0,0 +1,13 @@
+package com.app.exemploddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplodddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplodddApplication.class, args);
+	}
+
+}
